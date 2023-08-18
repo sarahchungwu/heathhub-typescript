4e6d8563c3dc9a1f56476a547fa61d1ce0cdf975
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-19 09:30:08 +1200

    have the layout of nav bar

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,37 @@
+import {  useState } from "react"
+import { Bars3Icon, XMarkIcon} from "@heroicons/react/20/solid"
+import Logo from "@/assets/Logo.png"
+
+type Props = {}
+
+const Navbar = (props:Props) => {
+  const flexBetween = "flex items-center justfiy-between"
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img src={Logo} alt="logo" />
+          </div>
+          {/* Right Side */}
+          <div className={`${flexBetween} w-full`}>
+            <div className={`${flexBetween} gap-8 text-sm`}>
+              <p>Home</p>
+              <p>Benifits</p>
+              <p>Our Classes</p>
+              <p>Contact us</p>
+            </div>
+            <div className={`${flexBetween} gap-8`}>
+              <p>Sign In</p>
+              <button>Become a Member</button>
+            </div>
+
+          </div>
+          
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
