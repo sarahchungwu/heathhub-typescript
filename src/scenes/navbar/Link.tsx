67fbb685ed3dc8ea9880406b97d0c55a7c5239aa
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-19 10:11:16 +1200

    intialize the functionality of link page for lowercase and click

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -2,11 +2,19 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
   page: string
+  selectedPage: string
+  setSelectedPage: (value:string) => void
 }
 
-const Link = ({page}: Props) => {
+const Link = ({page,selectedPage, setselectedPage}: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g,"")
   return (
-    <AnchorLink>
+    <AnchorLink
+    className={`${selectedPage === lowerCasePage? "text-primary-500": ""} transition duration-500 hover:text-primary-300` }
+    href={`#${lowerCasePage}`}
+    onClick={()=> setselectedPage(lowerCasePage)}
+
+    >
       {page}
     </AnchorLink>
   )
