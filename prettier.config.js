4fe4c4e35ebb36dc5952a9dbfe96cde58e61bbe6
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-19 14:21:52 +1200

    create type of enum

--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-import prettierPluginTailwindCSS from 'prettier-plugin-tailwindcss';
-
-export default {
-  plugins: [prettierPluginTailwindCSS],
-};
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import Navbar from "@/scenes/navbar"
-import { useState } from "react"
+import Navbar from '@/scenes/navbar'
+import { useState } from 'react'
 
+enum SelectedPage {
+  Home = 'home',
+  Benefits = 'benefits',
+  OurClasses = 'ourclasses',
+  ContactUs = 'contactus',
+}
 
 function App() {
-  const [selectedPage,setSelectedPage] = useState<string>('hone')
+  const [selectedPage, setSelectedPage] = useState<string>(SelectedPage.Home)
   return (
     <div className="app bg-gray-20">
-      <Navbar
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-    </div>)
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  )
 }
 
 export default App
