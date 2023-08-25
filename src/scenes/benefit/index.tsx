31a738db45d1cdc98145ff3f5aec29cf62bf6419
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-26 11:22:12 +1200

    created the benefit type and the benefits array

--- src/scenes/benefit/index.tsx
+++ src/scenes/benefit/index.tsx
@@ -1,5 +1,5 @@
 import HText from '@/shared/HText'
-import { SelectedPage } from '@/shared/types'
+import { BenefitType, SelectedPage } from '@/shared/types'
 import {
   AcademicCapIcon,
   HomeModernIcon,
@@ -10,6 +10,26 @@ import { motion } from 'framer-motion'
 type Props = { setSelectedPage: (value: SelectedPage) => void }
 
 const Benefits = ({ setSelectedPage }: Props) => {
+  const benefits: Array<BenefitType> = [
+    {
+      icon: <HomeModernIcon className="h-6 w-6" />,
+      title: 'State of the Art Facilities ',
+      description:
+        'Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.',
+    },
+    {
+      icon: <UserGroupIcon className="h-6 w-6" />,
+      title: "100's of Diverse Classes",
+      description:
+        'Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.',
+    },
+    {
+      icon: <AcademicCapIcon className="h-6 w-6" />,
+      title: 'Expert and Pro Trainers',
+      description:
+        'Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.',
+    },
+  ]
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
       <motion.div
@@ -23,6 +43,13 @@ const Benefits = ({ setSelectedPage }: Props) => {
             get you to your ultimate fitness goals with ease. We provide true
             careinto each and every member.
           </p>
+        </div>
+
+        {/* BENEFITS */}
+        <div className="md:flex items-center justify-between gap-8 ">
+          {benefits.map((benefit) => (
+            <Benefit />
+          ))}
         </div>
       </motion.div>
     </section>
