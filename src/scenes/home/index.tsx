80658cd4be1913d4893859bcb4719f3c09316740
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-26 10:17:15 +1200

    fixed the scolling back to top and the background pink disppear

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -17,7 +17,10 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full">
       {/* IMAGE AND MAIN HEADER */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div
+        className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* MAIN HEADEr */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
@@ -72,7 +75,8 @@ const Home = ({ setSelectedPage }: Props) => {
         <div className="flex basis-3/5 justify-center md:z-10 md:ml-48 md:mt-16 md:justify-items-end">
           <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
-      </div>
+      </motion.div>
+
       {/* SPONSERS */}
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
