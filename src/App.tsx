bb68cb1ec5f1ad7a9bc12337d5e331fac87aa343
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-25 18:29:16 +1200

    finished the scrolling event listener in the nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import Navbar from '@/scenes/navbar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { SelectedPage } from '@/shared/types'
 
 function App() {
   const [selectedPage, setSelectedPage] = useState(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScoll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      }
+
+      setIsTopOfPage(false)
+    }
+    window.addEventListener('scroll', handleScoll)
+    return () => window.removeEventListener('scroll', handleScoll)
+  }, [])
+
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
