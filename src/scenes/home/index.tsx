94db1c09d7a279086c36d07468106b169aff10d9
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-26 09:02:13 +1200

    created the relative position for the img for the text

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -16,13 +16,13 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full">
       {/* IMAGE AND MAIN HEADER */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* MAIN HEADEr */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <div className="md:mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
