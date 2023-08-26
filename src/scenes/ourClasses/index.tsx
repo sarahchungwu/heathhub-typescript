0c3969149dab02fdc2c0a6501812f0821bb77692
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-26 17:32:19 +1200

    created the class component and working on the row scolling

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,12 +1,48 @@
-import { SelectedPage } from '@/shared/types'
+import { ClassType, SelectedPage } from '@/shared/types'
 import image1 from '@/assets/image1.png'
 import image2 from '@/assets/image2.png'
 import image3 from '@/assets/image3.png'
 import image4 from '@/assets/image4.png'
 import image5 from '@/assets/image5.png'
-import image6 from '@/assets/image16.png'
+import image6 from '@/assets/image6.png'
 import { motion } from 'framer-motion'
 import HText from '@/shared/HText'
+import Class from './Class'
+
+const classes: Array<ClassType> = [
+  {
+    name: 'Weight Training Classes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: image1,
+  },
+  {
+    name: 'Yoga Classes',
+    image: image2,
+  },
+  {
+    name: 'Ab Core Classes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: image3,
+  },
+  {
+    name: 'Adventure Classes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: image4,
+  },
+  {
+    name: 'Fitness Classes',
+    image: image5,
+  },
+  {
+    name: 'Training Classes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: image6,
+  },
+]
 
 type Props = { setSelectedPage: (value: SelectedPage) => void }
 
@@ -37,6 +73,18 @@ const OurClasses = ({ setSelectedPage }: Props) => {
             </p>
           </div>
         </motion.div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
       </motion.div>
     </section>
   )
